8295dc9299dc2c4323f5607018f3bf6606e0e785
Ross Edwards <rossedwards@Rosss-MacBook-Pro.local>
2018-03-13 22:09:48 -0700

    manage projects

--- web/__init__.py
+++ web/__init__.py
@@ -42,7 +42,7 @@ def build_app(settings_path, loop=None):
     if settings['assets']['use_proxy']:
         application.assets = AssetManager(application, prefix=settings['assets']['base_path'])
     else:
-        static_dir = os.path.join(os.path.dirname(here_folder), 'public')
+        static_dir = os.path.join(os.path.dirname(here_folder), 'static')
         application.assets = AssetManager(
             application,
             prefix=settings['assets']['base_path'],
